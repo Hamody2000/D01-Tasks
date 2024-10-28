c7cfe5bdfd6268d4106737bb5369a5c7b7473278
Hamody <ahmed.hamody5532@gmail.com>
2024-10-28 03:19:52 +0300

    task D01

---
+++ Task/Program.cs
@@ -0,0 +1,138 @@
+﻿using System.Globalization;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //int year = 0, month = 0, day = 0;
+
+            //Console.WriteLine("plz enter your year from 1980->2023");
+
+            //year = int.Parse(Console.ReadLine());
+            //while (year < 1980 || year > 2023)
+            //{
+            //    Console.WriteLine("plz enter your year from 1980->2023");
+            //    year = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine(year);
+
+            //do
+            //{
+            //    Console.WriteLine("plz enter your month 1-12");
+            //    month = int.Parse(Console.ReadLine());
+
+            //    if (month < 1 || month > 12)
+            //    {
+            //        Console.WriteLine("plz enter your month 1-12");
+            //        month = int.Parse(Console.ReadLine());
+            //    }
+            //} while (month < 1 || month > 12);
+
+            //int[] monthsWith31Days = { 1, 3, 5, 7, 8, 10, 12 };
+            //int[] monthsWith30Days = { 4, 6, 9, 11 };
+
+            //do
+            //{
+            //    Console.WriteLine("plz enter your day");
+            //    day = int.Parse(Console.ReadLine());
+            //    if (monthsWith30Days.Contains(month) && day == 31)
+            //    {
+            //        Console.WriteLine("plz enter your day");
+            //        day = int.Parse(Console.ReadLine());
+
+            //    }
+            //} while (day > 31 || day < 0);
+
+            //Console.WriteLine($"you are {2024 - year}y {10 -month}months { 21 - day} days");
+            #endregion
+
+            //char continu = 'y';
+            //char stop = 'n';
+            //while (continu == 'y')
+            //{
+            //    Console.WriteLine("plz enter #1");
+            //    int x = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("plz enter #2");
+            //    int y = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("plz enter operator");
+            //    char operate = char.Parse(Console.ReadLine());
+
+            //    double result = 0;
+            //bool validOperator = true;
+
+            //    switch (operate)
+            //    {
+            //        case '+':
+            //            result = x + y;
+            //            break;
+            //        case '-':
+            //            result = x - y;
+            //            break;
+            //        case '*':
+            //            result = x * y;
+            //            break;
+            //        case '/':
+            //            if (y != 0)
+            //            {
+            //                result = x / y;
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Error: Division by zero is not allowed.");
+
+            //            }
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid operator! Please enter +, -, *, or /.");
+            //            validOperator = false;
+            //            break;
+            //    }
+            //    if (validOperator)
+            //    {
+            //        // Display result
+            //    Console.WriteLine($"{x} {operate} {y} = {result}");
+            //    }
+            //    Console.WriteLine("continue? y or n");
+            //    continu = char.Parse(Console.ReadLine()); 
+            //}
+            //Console.WriteLine("end program");
+
+
+            //int[] arr = { 1, 2, 25, 12, 3, 5, 6, 7, 8, 9, 10, 11, 1, 6, 2, 3 };
+            //Console.WriteLine("which number you want to search");
+            //int search = int.Parse(Console.ReadLine());
+            //int index = Array.IndexOf(arr, search);
+            //Console.WriteLine($"Number {search} found at index {index}");
+
+
+            //int[] arr = { 3, 4, 5, 6, 7, 8,650, 9, 10, 11, 1, 12 };
+            //int max = arr[0];
+            //int min = arr[0];
+
+            //for (int i = 0; i < arr.Length; i++) {
+
+            //    if (arr[i] > max) { 
+            //      max = arr[i]; 
+            //    };
+
+            //    if (arr[i] < min)
+            //    {
+            //        min = arr[i];
+            //    }
+
+            //}
+            //Console.WriteLine($"max number is {max} min number is {min}");
+
+
+            ///////////////////////////////////////////////////////////////
+
+
+
+        }
+    }
+}
